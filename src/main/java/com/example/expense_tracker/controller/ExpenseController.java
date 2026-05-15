fc5d261769652233e87a2ed4f9bd6e5f9639c33c
Tandrita100 <tandrita29@gmail.com>
2026-05-15 16:01:03 +0530

    Added controller and implemented create/read expense APIs

--- src/main/java/com/example/expense_tracker/controller/ExpenseController.java
+++ src/main/java/com/example/expense_tracker/controller/ExpenseController.java
@@ -1,4 +1,32 @@
 package com.example.expense_tracker.controller;
 
+import com.example.expense_tracker.entity.Expense;
+import com.example.expense_tracker.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/expenses")
 public class ExpenseController {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @PostMapping
+    public Expense addExpense(@RequestBody Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    @GetMapping
+    public List<Expense> getAllExpenses(){
+        return expenseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Expense getExpenseById(@PathVariable Long id){
+        return expenseRepository.findById(id).orElse(null);
+    }
+
 }
